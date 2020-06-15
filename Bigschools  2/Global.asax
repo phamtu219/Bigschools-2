2a01c3d0459ac3cd9fa28a12ae8cb84a7de3c6c5
Aku <Aku@DESKTOP-72BKL7Q>
2020-06-15 20:37:25 +0700

    Add project files.

---
+++ Bigschools  2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bigschools__2.MvcApplication" Language="C#" %>
---
+++ Bigschools  2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bigschools__2.Startup))]
+namespace Bigschools__2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
